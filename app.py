d81fc35d17278ebd2dc4c3eae5542a0f67f64409
sysani <russo.anitaa@gmail.com>
2020-07-28 11:47:08 -0400

    updating project to use postgresql

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:PGDb512@localhost/py_data'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db=SQLAlchemy(app)
+
+class Data(db.Model):
+    __tablename__="data"
+    id=db.Column(db.Integer, primary_key=True)
+    email=db.Column(db.String(120), unique=True)
+    height=db.Column(db.Integer)
+
+    def __init__(self, email, height):
+        self.email=email
+        self.height=height
+
 @app.route("/")
 def index():
     return render_template("index.html")
