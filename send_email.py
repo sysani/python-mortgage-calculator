8c2f1cf6a38a62a9583f5ef0893df31bc152c641
sysani <russo.anitaa@gmail.com>
2020-08-14 10:33:53 -0400

    fixed email formatting

--- send_email.py
+++ send_email.py
@@ -8,7 +8,7 @@ def send_email(email, location, down, salary, total):
     from_email="annie@null.net"
     to_email=email
     subject="Real Estate Data"
-    message="With a down payment of $<b>%s</b>and a salary of $<b>%s,</b> " % (down, salary)
+    message="With a down payment of $<b>%s</b> and a salary of $<b>%s,</b> " % (down, salary)
     message+="you could afford a $<b>%s</b> home in %s" % (total, location)
     #message+="<br>More Information:<br>Average Down Payment:%s<br>Average Salary:%s<br>" % (downpaymnent_avg, salary_avg)
     msg=MIMEText(message, 'html')
